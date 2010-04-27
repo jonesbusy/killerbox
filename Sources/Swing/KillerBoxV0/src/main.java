eeb306cfd349b3cab1bad61721f837e54fb8280b
fabrizioberettap@gmail.com <fabrizioberettap@gmail.com@2091e038-e186-4e30-6398-17741e504ea4>
2010-04-27 19:59:20 +0000

    panels

--- Sources/Swing/KillerBoxV0/src/main.java
+++ Sources/Swing/KillerBoxV0/src/main.java
@@ -2,7 +2,7 @@
 public class main 
 {
 	private static int hauteur = 400;
-	private static int largeur = 400;
+	private static int largeur = 430;
 	
 	/**
 	 * @param args
@@ -10,13 +10,7 @@ public class main
 	public static void main(String[] args) 
 	{
 		
-		//FenetreBase fb 			= new FenetreBase(hauteur,largeur);
-		//FenetrePrincipale fp 		= new FenetrePrincipale(hauteur,largeur);
-		//FenetreUser fu			= new FenetreUser(hauteur,largeur);
-		//FenetreAdmin fa 			= new FenetreAdmin(hauteur, largeur);
-		//FenetreConnection fc		= new FenetreConnection(hauteur,largeur);
-		//FenetreComptes fcptes		= new FenetreComptes(hauteur,350);
-		//FenetreTypePartie ft		= new FenetreTypePartie(hauteur,largeur);
-		FenetreCreationCompte fcc	= new FenetreCreationCompte(hauteur,largeur);
+		FenetreBase fb 			= new FenetreBase(hauteur,largeur);
+
 	}
 }
